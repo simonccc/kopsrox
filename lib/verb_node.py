fd9cb964e00744c559affc19398170a30f509e53
simonccc <simonc@gmail.com>
2023-05-26 20:54:18 +0100

    add new files

---
+++ lib/verb_node.py
@@ -0,0 +1,19 @@
+import common_config as common, sys, os, wget, re, time, urllib.parse
+verb = 'node'
+verbs = common.verbs_node
+
+# check for arguments
+try:
+  if (sys.argv[2]):
+    passed_verb = str(sys.argv[2])
+except:
+  print('ERROR: pass a command')
+  print(verb, '', end='')
+  common.verbs_help(verbs)
+  exit(0)
+
+# unsupported verb
+if not passed_verb in verbs:
+  print('ERROR:\''+ passed_verb + '\'- command not found')
+  print('kopsrox', verb, '', end='')
+  common.verbs_help(verbs)
