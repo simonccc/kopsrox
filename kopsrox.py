4dbc2c36816d1a53aec365024f9f60feaeff9e7b
simonccc <simonc@gmail.com>
2024-04-03 19:28:45 +0100

    create utility node

--- kopsrox.py
+++ kopsrox.py
@@ -35,6 +35,7 @@ cmds = {
   },
   "node": {
     "destroy" : 'hostname',
+    "utility" : '',
   },
 }
 
--- lib/verb_node.py
+++ lib/verb_node.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 # functions
-from kopsrox_config import vmnames,cluster_info, kmsg_err, kmsg_sys
+from kopsrox_config import vmnames,cluster_info, kmsg_err, kmsg_sys, cluster_id, list_kopsrox_vm, kmsg_info
 from kopsrox_k3s import k3s_rm
+from kopsrox_proxmox import clone
 
 # other imports
 import sys
 
 # passed command
 cmd = sys.argv[2]
-arg = sys.argv[3]
+
+# map arg if passed
+try:
+  arg = sys.argv[3]
+except:
+  pass
 
 # define kname
 kname = 'node-'+cmd
@@ -27,4 +33,14 @@ if cmd == 'destroy':
 
   # passed hostname not found 
   kmsg_err(kname, (arg+ ' vm not found'))
+  cluster_info()
+
+# create utility node
+if cmd == 'utility':
+  uid = cluster_id + 4 
+  if uid not in list_kopsrox_vm():
+    kmsg_sys(kname, 'creating utility node')
+    clone(uid)
+  else:
+    kmsg_info(kname, 'utility node already exists')
   cluster_info()
