a2b0aa930405144ad47d15d2882ba73503bc7b98
simonccc <simonc@gmail.com>
2024-08-24 22:12:07 +0100

    remove dupe

--- lib/kopsrox_k3s.py
+++ lib/kopsrox_k3s.py
@@ -315,7 +315,6 @@ def get_kube_vip_master():
     kmsg('kubevip_check', 'no kubevip label found - reloading kubevip', 'err')
     kubevip_r = kubectl('rollout restart daemonset kube-vip-ds  -n kube-system')
     time.sleep(1)
-    kubevip_q = f'get nodes --selector kube-vip.io/has-ip={network_ip} 2>&1'
     kubevip_o = kubectl(kubevip_q)
     #print(kubevip_o.split())
     kubevip_m = kubevip_o.split()[5]
